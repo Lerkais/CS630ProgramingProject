bede47159ccb49d287c4872d8966f8d91cc23215
Lerkais <66885833+Lerkais@users.noreply.github.com>
2024-02-17 00:02:26 +0000

    Initial Commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from simpy import *
+#Group 1 CS 630
+#Changes and participation can be seen in github
+#If creating new function, create in a new file or a file only edited by you
+#This helps prevent merge conflicts
+#James will edit your function into this main file as needed
+
+def main():
+    print("hello world")
+
+
+main()
