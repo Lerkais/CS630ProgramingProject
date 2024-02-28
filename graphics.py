62b0b3903a5d961891ed2600dc1619b4ba5d6a82
Lerkais <66885833+Lerkais@users.noreply.github.com>
2024-02-28 00:37:53 +0000

    Changed things

--- graphics.py
+++ graphics.py
@@ -1,3 +1,3 @@
 def displayGnattChart(listOfProcessActiveTimes):
-    #Format will be [[(s1,e1),(s2,e2)],[(s,e)]] etc
+    #Format will be jobID:cyclesActive,repeat etc
     pass
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from simpy import *
 from random import *
+import copy
 #Group 1 CS 630
 #Changes and participation can be seen in github
 #If creating new function, create in a new file or a file only edited by you
@@ -9,11 +10,15 @@ from random import *
 seed(103)
 #IO Distributions
 def randomIO():
-    random()*500+100
+    return randint(1,500)+100
 
 #Time Distributions
 def randomTime():
-    random()*100+100
+    return randint(1,100)+100
+
+#Job Request Time Distributions
+def randomRequestTime():
+    return randint(1,1000)
 
 #Magic Numbers
 numJobs = 10
@@ -22,53 +27,80 @@ TimeDist = randomTime
 chanceForIO = .3
 IODist = randomIO
 
+#Storage mechanisms
 
 
-class job:
-    timeRemaing = 0
-    requestTime = 0
-    startTime = 0
-    completionTime = 0
-    activeTimeChart = []
-    IO = False
-    IORequestTime = 0
-    IOWaitTime = 0
-    def __init__(self,time) -> None:
-        self.timeRemaing = time;
-        if random() <= chanceForIO:
-            self.IO = True
-            self.IORequestTime = time*random()
-            self.IOWaitTime = IODist()
+
 
 results = []
-    
-def FCFS(env,jobs):
+
+class job:
+    def __init__(self,time):
+        self.timeRemaing = time()
+        self.requestTime = 0
+        self.jid = 0
+        self.done = False
+        self.timeGraph = []
+
+    def step(self,env,timeGraph):
+        self.timeRemaing -= 1
+        timeGraph.append(self.jid)
+        if self.timeRemaing <= 0:
+            self.done = True
+        else:
+            self.done = False
+        yield env.timeout(1)
+
+def timeGraphToString(timeGraph):
+    cj = timeGraph[0];
+    total = 0
+    strtoprnt = ""
+    for i in timeGraph:
+        if i == cj:
+            total += 1
+        else:
+            strtoprnt += str(cj) + ":" + str(total) + ","
+            total = 0
+            cj = i
+    print(strtoprnt)
+
+
+
+def FCFS(env,jobs,timeGraph):
+    jobs = sorted(jobs,key=lambda x: x.requestTime)
+    print(len(jobs))
     for j in jobs:
-        j.startTime = env.now
-        j.activeTimeChart.append((env.now,-1))
-        if(j.IO):
-            yield env.timeout(j.IORequestTime)
-            j.activeTimeChart[-1][1] = env.now
-            j.timeRemaing -= j.IORequestTime
-            yield env.timeout(j.IOWaitTime)
-            j.activeTimeChart.append((env.now,0))
-        yield env.timeout(j.timeRemaing)
-        j.activeTimeChart[-1][1] = env.now
-        j.completionTime = env.now
+        while not j.done:
+            yield env.process(j.step(env,timeGraph))
+        
     results.append(jobs)
+
+
+
 
 functs = [FCFS]
 def JobDispatcher(env):
-    jobs = [job(TimeDist()) for i in range(numJobs)]
+    jobs = [job(TimeDist) for i in range(numJobs)]
     for j in jobs:
-        j.IO = random() <= chanceForIO
-    for funct in functs:
-        yield env.process(funct(env,jobs.copy()))
-    
+        j.requestTime = randomRequestTime()   
+        j.jid = jobs.index(j);
+        j.done = False  
+
+    for algo in functs:
+        timeGraph = []
+        yield env.process(algo(env,copy.deepcopy(jobs),timeGraph))
+        timeGraphToString(timeGraph)
+    pass
+
+def test(env):
+    yield env.timeout(1);
+    yield env.process(JobDispatcher(env))
+
 
 def main():
-    print("hello world")  
+    #print("hello world")
     env = Environment()
-    env.process(JobDispatcher(env))
+    env.process(test(env))
+    env.run()
 
 main()
