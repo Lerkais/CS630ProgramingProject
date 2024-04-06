40f74b90e4af0227273d0b2403133a026c6110ae
J5432154 <145301782+J5432154@users.noreply.github.com>
2024-04-06 13:34:54 -0400

    Update front_end_graphics.py

--- front_end_graphics.py
+++ front_end_graphics.py
@@ -1,9 +1,9 @@
-#4.4.24
+#4.6.24
 #The point of this program is to great a front end for the user to interact with.
 import customtkinter #A custom made more updated version of tkinter
 
 def button_event(): #defines action when button is pressed #should work fine as standaline
-    if check_var1.get() == "on" or check_var2.get() == "on" or check_var3.get() == "on" or check_var4.get() == "on" or check_var5.get() == "on": #first checks condition if nothing was clicked
+    if check_var1.get() == "on" or check_var2.get() == "on" or check_var3.get() == "on" or check_var4.get() == "on" or check_var5.get() == "on" or check_var6.get() == "on": #first checks condition if nothing was clicked
         label.configure(text="You clicked something!") #checks to see if check mark was clicked
     else:
         label.configure(text="You did not click anything.")
@@ -23,29 +23,70 @@ def check_box(text_input = "Algorithm", pady=20):
     check.pack(pady=pady) #first box
     return check_var
 
-def enter_button(button_label = "Enter", relx = 0.5, rely = .9, pady = 60):
+def enter_button(button_label = "Enter", relx = 0.5, rely = .9, pady = 60, eventAction = button_event):
     global label
-    enter = customtkinter.CTkButton(master=mainMenu, text = button_label, command=button_event) #main button user presses when they want to enter
+    enter = customtkinter.CTkButton(master=mainMenu, text = button_label, command=eventAction) #main button user presses when they want to enter
     enter.place(relx=relx, rely=rely, anchor=customtkinter.CENTER) #defines where the button goes
     label = customtkinter.CTkLabel(mainMenu, text="")
     label.pack(pady=pady)
+
+def arrivalValue(input): #sliding
+    arrivalLabel.configure(text=int(input)) #value getting passed by slider widget
+
+def burstValue(input):
+    burstLabel.configure(text=int(input))
+
+def tupleAction(v1= ()):
+    print(v1)
+    return v1
         
 def createMainScreen(): #call this if a program wants the interface
-    global check_var1, check_var2, check_var3, check_var4, check_var5 #(declares variables so they
+    global check_var1, check_var2, check_var3, check_var4, check_var5, check_var6 #(declares variables so they
     #can communicate with other methods)
 
     main_window() #generates the main window
 
     check_var1 = check_box("FCFS", 20) #first box
 
-    check_var2 = check_box("SRT", 24) #second box
+    check_var2 = check_box("SRT", 21) #second box
 
-    check_var3 = check_box("RR", 28) #third box
+    check_var3 = check_box("RR", 22) #third box
 
-    check_var4 = check_box("SPN", 32) #fourth box
+    check_var4 = check_box("SPN", 23) #fourth box
 
-    check_var5 = check_box("HRRN", 36) #fifth box
+    check_var5 = check_box("HRRN", 24) #fifth box
+
+    check_var6 = check_box("FB", 25) #sixth box
 
     enter_button() #allows user to send input to be analyzed
 
     mainMenu.mainloop() #displays main screen and responds to inputs
+
+def arrivalBurstTimeInput():
+    main_window()
+    arrivalSlider = customtkinter.CTkSlider(mainMenu, from_=0, to=100, command=arrivalValue)
+    arrivalSlider.pack(pady = 20)
+    global arrivalLabel
+    arrivalLabel = customtkinter.CTkLabel(mainMenu, text=arrivalSlider.get(), font=("Helvetica", 18))
+    arrivalLabel.pack(pady = 40)
+    arrivalSlider.set(0)
+
+
+
+    burstSlider = customtkinter.CTkSlider(mainMenu, from_= 0, to=100, command=burstValue)
+    burstSlider.pack(pady = 60)
+    global burstLabel
+    burstLabel = customtkinter.CTkLabel(mainMenu, text=burstSlider.get(), font=("Helvetica", 18))
+    burstLabel.pack(pady=70)
+    burstSlider.set(0)
+    #tupleStorage = (arrivalSlider.get(), burstSlider.get()) #work on tuples later, first get functionality of connecting the two screens
+    enter_button(eventAction=mainMenu.destroy) #resolve the error codes later
+    mainMenu.mainloop()
+    
+
+#revise mainMenu to something else unique to the first tab to see if errors can be avoided
+#arrivalBurstTimeInput() #work on modularizing it and making two separate #when trying to run back to back, creates weird text in terminal
+#createMainScreen()
+#createMainScreen()
+
+
