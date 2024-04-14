777928afde96c54919dbcdb75c7334714292d271
J5432154 <145301782+J5432154@users.noreply.github.com>
2024-04-14 12:54:54 -0400

    front_end_graphics.py
    Meets front_end requirements and could use minor code adjustments and formatting screen options.

--- front_end_graphics.py
+++ front_end_graphics.py
@@ -1,6 +1,7 @@
-#4.6.24 4.13.24
+#4.6.24 4.13.24 4.14.24
 #The point of this program is to great a front end for the user to interact with.
 import customtkinter #A custom made more updated version of tkinter
+
 
 def button_event(): #defines action when button is pressed #should work fine as standaline
     if check_var1.get() == "on" or check_var2.get() == "on" or check_var3.get() == "on" or check_var4.get() == "on" or check_var5.get() == "on" or check_var6.get() == "on": #first checks condition if nothing was clicked
@@ -41,7 +42,7 @@ def burstValue(input):
 
 def jobsValue(input): #sliding
     numberOfJobsLabel.configure(text=int(input)) #value getting passed by slider widget
-
+    
 
 def tupleAction(v1= ()):
     print(v1)
@@ -50,7 +51,6 @@ def tupleAction(v1= ()):
 def createMainScreen(): #call this if a program wants the interface
     global check_var1, check_var2, check_var3, check_var4, check_var5, check_var6 #(declares variables so they
     #can communicate with other methods)
-
     main_window() #generates the main window
 
     begin_label = customtkinter.CTkLabel(mainMenu, text="Welcome to the Operating System Algorithm Selector.\n To start the program please check the algoritm(s) you would like to test.", font=("Helventica", 18))
@@ -72,50 +72,79 @@ def createMainScreen(): #call this if a program wants the interface
 
     mainMenu.mainloop() #displays main screen and responds to inputs
 
-def arrivalBurstTimeInput():
-    main_window()
-    arrival_label_input = customtkinter.CTkLabel(mainMenu, text="Please scroll to adjust the arrival time of the job.\nArrival Time:", font=("Helventica", 18))
-    arrival_label_input.pack(pady = 20)
-    arrivalSlider = customtkinter.CTkSlider(mainMenu, from_=0, to=100, command=arrivalValue)
-    arrivalSlider.pack(pady = 25)
-    global arrivalLabel
-    arrivalLabel = customtkinter.CTkLabel(mainMenu, text=arrivalSlider.get(), font=("Helvetica", 18))
-    arrivalLabel.pack(pady = 40)
 
-    burst_label_input = customtkinter.CTkLabel(mainMenu, text="Please scroll to adjust the burst time of the job.\nBurst Time:", font=("Helventica", 18))
-    burst_label_input.pack(pady = 55)
-    burstSlider = customtkinter.CTkSlider(mainMenu, from_= 0, to=100, command=burstValue)
-    burstSlider.pack(pady = 60)
-    global burstLabel
-    burstLabel = customtkinter.CTkLabel(mainMenu, text=burstSlider.get(), font=("Helvetica", 18))
-    burstLabel.pack(pady=65)
-    #tupleStorage = (arrivalSlider.get(), burstSlider.get()) #work on tuples later, first get functionality of connecting the two screens
-    enter_button(eventAction=mainMenu.destroy) #resolve the error codes later
-    mainMenu.mainloop()
+
+def arrivalBurstTimeInput():
+    arrivalListStorage = []
+    burstListStorage = []
+    counter = 0
+    
+    while numberOfJobsInput > counter:
+        main_window()
+        print(counter)
+        arrival_label_input = customtkinter.CTkLabel(mainMenu, text="Please scroll to adjust the arrival time of the job.\nArrival Time:", font=("Helventica", 18))
+        arrival_label_input.pack(pady = 20)
+        arrivalSlider = customtkinter.CTkSlider(mainMenu, from_=0, to=100, command=arrivalValue)
+        arrivalSlider.pack(pady = 25)
+
+        global arrivalLabel
+        arrivalLabel = customtkinter.CTkLabel(mainMenu, text=arrivalSlider.get(), font=("Helvetica", 18))
+        arrivalLabel.pack(pady = 40)
+
+        burst_label_input = customtkinter.CTkLabel(mainMenu, text="Please scroll to adjust the burst time of the job.\nBurst Time:", font=("Helventica", 18))
+        burst_label_input.pack(pady = 55)
+        burstSlider = customtkinter.CTkSlider(mainMenu, from_= 1, to=101, command=burstValue)
+        burstSlider.pack(pady = 60)
+
+        global burstLabel
+        burstLabel = customtkinter.CTkLabel(mainMenu, text=burstSlider.get(), font=("Helvetica", 18))
+        burstLabel.pack(pady=65)
+        enter_button(eventAction=mainMenu.destroy) #resolve the error codes later
+        mainMenu.mainloop()
+        arrivalListStorage.append(str(int(arrivalSlider.get())))
+        burstListStorage.append(str(int(burstSlider.get())))
+        counter += 1
+        
+    print(arrivalListStorage)
+    print(burstListStorage)
+    tupleStorageValues = list(zip(arrivalListStorage, burstListStorage))
+    print(tupleStorageValues)
+    return tupleStorageValues #returns tuple value
+    
+
+    #work on tuples later, first get functionality of connecting the two screens
+    
     
 def numberOfJobs():
     main_window()
     begin_label = customtkinter.CTkLabel(mainMenu, text="Please select the number of jobs you would like to process.\nOnce selected, please press enter to continue.", font=("Helventica", 18))
     begin_label.pack(pady = 20)
-    numberOfJobsSlider = customtkinter.CTkSlider(mainMenu, from_=0, to=100, command=jobsValue)
+    numberOfJobsSlider = customtkinter.CTkSlider(mainMenu, from_=1, to=101, command=jobsValue)
     numberOfJobsSlider.pack(pady = 50)
+
     global numberOfJobsLabel
     numberOfJobsLabel = customtkinter.CTkLabel(mainMenu, text=numberOfJobsSlider.get(), font=("Helvetica", 18))
     numberOfJobsLabel.pack(pady = 40)
-
+    global numberOfJobsInput
     enter_button(eventAction=mainMenu.destroy)
     mainMenu.mainloop()
+    numberOfJobsInput = int(numberOfJobsSlider.get())
 
-#createMainScreen() #intended order to run
-#numberOfJobs()   
-#arrivalBurstTimeInput()
-
-#stuff works just adjust
+#intended order of execution  
+#createMainScreen() #modularize beginning label
+#numberOfJobs()   #modularize beginning label, (modularize the sliders?)
+#arrivalBurstTimeInput() #modularize beginning label, (modularize the sliders?)
+ 
 
 
 #things to do: 
-#save input for number of jobs
-#work on saving the information in a tuple for burst time and arrvial
-#create loop that reloads the burst/arrival time based on the number of jobs inputted
+
+#try to modularize the code? (sliders)
+#try to make double click a single click
+#try to minimize the use of global variables and rely more on returns
+#format the windows visuals
+#remove debugging code
+
+
 
 
